d49f964e7d4d2e442a9d74ef3e5ed09cd51bc4ab
Charlotte Strickland <charlottemarie81@gmail.com>
2022-08-24 22:34:11 -0400

    setup game

---
+++ src/Components/empty_slot.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import hillImg from './molehill.png';
+
+const EmptySlot = (props) => {
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.toggle(true)
+        }, randSeconds)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}} src={hillImg} />
+        </div>
+    )
+}
+
+export default EmptySlot
